489f14f7597d241546e20d103662ec26a880ae92
Pranav Murali <pranavmurali024@gmail.com>
2024-03-19 11:19:09 +0530

    redirects to form

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
       </h1>
       <h2 className="text-3xl px-8 text-left text-gray-600 ml-12">Your personal AI investment advisor</h2>
       <a
-      href="./form1.html"
+      href="http://localhost:3000/Form"
       className="inline-flex items-center px-4 py-2 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 justify-center mt-12" // Added justify-center class
     >
       Start Investing &#8594;
